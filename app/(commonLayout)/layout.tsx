9d1ea1946e1eb016665e3c7a33a9d4f7fef834d7
Rdm-jony <jonydascse@gmail.com>
2025-11-23 19:05:04 +0600

    first commit

---
+++ app/(commonLayout)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FeedLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            {children}
+        </>
+    );
+};
+
+export default FeedLayout;
