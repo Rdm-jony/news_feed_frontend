c918c5949caa7971ee233a1ec00a42735edea75c
Rdm-jony <jonydascse@gmail.com>
2025-11-25 19:43:20 +0600

    refactor code in proxy

--- proxy.ts
+++ proxy.ts
@@ -1,49 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-import { deleteCookie, getCookie } from "./lib/tokenHandlers";
 
 const protectedRoutes = ["/"];
 const publicRoutes = ["/signIn", "/register"];
 
-export async function proxy(req: NextRequest) {
-    const { pathname } = req.nextUrl;
+export function proxy(req: NextRequest) {
+  const { pathname } = req.nextUrl;
 
-    const token = await getCookie("accessToken");
+  const token = req.cookies.get("accessToken")?.value;
 
-    let decoded: any = null;
+  let decoded: any = null;
 
-    if (token) {
-        try {
-            decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!);
-        } catch (err) {
-            console.log("JWT verification failed:", err);
+  if (token) {
+    try {
+      decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!);
+    } catch (err) {
+      console.log("JWT verification failed:", err);
 
-            const response = NextResponse.redirect(new URL("/signIn", req.url));
-            deleteCookie("accessToken");
-            deleteCookie("refreshToken");
-            return response;
-        }
+      const response = NextResponse.redirect(new URL("/signIn", req.url));
+      response.cookies.delete("accessToken");
+      response.cookies.delete("refreshToken");
+      return response;
     }
+  }
 
-    const isProtected = protectedRoutes.some((route) =>
-        pathname === route
-    );
-    const isPublic = publicRoutes.some((route) =>
-        pathname === route
-    );
+  const isProtected = protectedRoutes.includes(pathname);
+  const isPublic = publicRoutes.includes(pathname);
 
-    if (isProtected && !decoded) {
-        return NextResponse.redirect(new URL("/signIn", req.url));
-    }
+  if (isProtected && !decoded) {
+    return NextResponse.redirect(new URL("/signIn", req.url));
+  }
 
-    if (isPublic && decoded) {
-        return NextResponse.redirect(new URL("/", req.url));
-    }
+  if (isPublic && decoded) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/", "/signIn", "/register"],
+  matcher: ["/", "/signIn", "/register"],
 };
